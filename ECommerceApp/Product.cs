f6c236a173f938833c52060a711950b2b3db95dd
Femosky <olufemiojeyemi@gmail.com>
2024-06-02 18:56:06 -0400

    Product.cs class file completed

--- ECommerceApp/Product.cs
+++ ECommerceApp/Product.cs
@@ -6,7 +6,39 @@ using System.Threading.Tasks;
 
 namespace ECommerceApp
 {
-    internal class Product
+    public class Product
     {
+        public int ProductID { get; set; }
+        public string ProductName { get; set; }
+        public double Price { get; set; }
+        public int Stock { get; set; }
+
+        public Product(int productID, string productName, double price, int stock)
+        {
+            ProductID = productID;
+            ProductName = productName;
+            Price = price;
+            Stock = stock;
+        }
+
+        public void IncreaseStock(int amount)
+        {
+            var addedValue = Stock + amount;
+
+            if (amount >= 0 && addedValue <= 1000)
+            {
+                Stock += amount;
+            }
+        }
+
+        public void DecreaseStock(int amount)
+        {
+            var decreasedValue = Stock - amount;
+
+            if (amount > 0 && decreasedValue >= 0)
+            {
+                Stock -= amount;
+            }
+        }
     }
 }
